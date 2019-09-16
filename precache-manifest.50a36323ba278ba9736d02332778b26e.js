dd2b2b09d543f69d222d1921007b0359ab8a3bcb
Paul Franco <paulfranco@me.com>
2019-09-15 17:07:37 -0700

    Updates

---
+++ precache-manifest.50a36323ba278ba9736d02332778b26e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77ae10299db22c8ac6904a86f69c5f3",
+    "url": "/monsters-roladex/index.html"
+  },
+  {
+    "revision": "c6b2513af1b5eae45c00",
+    "url": "/monsters-roladex/static/css/main.36395244.chunk.css"
+  },
+  {
+    "revision": "0f93ef1cd30d6e968da9",
+    "url": "/monsters-roladex/static/js/2.30c8aa33.chunk.js"
+  },
+  {
+    "revision": "c6b2513af1b5eae45c00",
+    "url": "/monsters-roladex/static/js/main.02faf4b4.chunk.js"
+  },
+  {
+    "revision": "e33b24d3f71ab376cabe",
+    "url": "/monsters-roladex/static/js/runtime~main.21bbf175.js"
+  }
+]);
